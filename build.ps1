132283f4fb42112fdc26ec3d2177aba32484a5a8
infinte-cloisters <lipase@foxmail.com>
2026-03-27 16:53:53 +0800

    update shell

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+make -C sphinx clean
 make -C sphinx html
 rm ./docs/* -r -fo
+
+# Create .nojekyll to prevent GitHub Pages from ignoring files and directories that start with an underscore
+echo "" > ./docs/.nojekyll
 cp -r sphinx/_build/html/* ./docs/
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+make -C sphinx clean
 make -C sphinx html
 rm -rf ./docs/*
+touch ./docs/.nojekyll
 cp -r sphinx/_build/html/* ./docs/
